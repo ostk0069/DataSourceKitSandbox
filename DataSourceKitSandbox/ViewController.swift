d446863e028a06ad6e4bac33fa4d774cc1b5c7e6
Takuma Osada <judgment9ttk069@gmail.com>
2019-05-30 20:46:52 +0900

    init default files

---
+++ DataSourceKitSandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataSourceKitSandbox
+//
+//  Created by Takuma Osada on 2019/05/30.
+//  Copyright © 2019 JX PRESS Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
